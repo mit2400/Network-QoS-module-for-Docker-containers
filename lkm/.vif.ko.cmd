9a62d70a5a7e15a7a5b2b525f5d9fe154655d3c0
minkoo <mit2400@naver.com>
2018-04-09 15:07:17 +0900

    git ignore add

--- lkm/.vif.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/vs/mkkang/lkm/vif.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/vs/mkkang/lkm/vif.ko /home/vs/mkkang/lkm/vif.o /home/vs/mkkang/lkm/vif.mod.o ;  true
